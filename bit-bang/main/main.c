e11ccec097360dac5bde0e77d11c4765ab14f0a1
umar <umarudy@outlook.co.id>
2021-05-13 21:08:49 +0700

    Explore interrupt

--- bit-bang/main/main.c
+++ bit-bang/main/main.c
@@ -4,7 +4,7 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
-
+#include <time.h>
 
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
@@ -43,6 +43,7 @@ const int data_pin   = 17; // DIO
 #include "driver/periph_ctrl.h"
 #include "driver/timer.h"
 #include "freertos/task.h"
+#include <sys/time.h>
 
 #include "esp_timer.h"
 #include "esp_intr_alloc.h"
@@ -135,8 +136,8 @@ static void init_sdcard() // tested on esp32 cam
   sdmmc_host_t host = SDMMC_HOST_DEFAULT();
   sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
   esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-      .format_if_mount_failed = false,
-      .max_files = 3,
+    .format_if_mount_failed = false,
+    .max_files = 3,
   };
   sdmmc_card_t *card;
 
@@ -172,75 +173,76 @@ typedef struct  {
 	int     ctr ;
 } state_t;
 
+struct timeval tv_now;
+
 #define AMAX 2000
 state_t astb[ AMAX ], aclk[ AMAX ], adat[ AMAX ];
 int cstb=0, cclk=0, cdat=0 ; //counter array stb
 
 void read_bitbybit(void *pvParameter) {
+	time_t t;
+  struct tm tm;
+  
+	int64_t time_us;
+
 	uint8_t clk, clk0=0, clks , dat , dat0=0, dats, stb, stb0=0 , stbs; // clks = clkStatus
 	uint64_t timer_start , ts =0, tc, td, ctr=1;
-	char *state[] = { "LOW", "RISING", "FALING", "HIGH", "DEF" };
+	char *state[] = { "LOW", "RISING", "FALING", "HIGH", "BLANK" };
 	int i = 0;
-	for(i=0;i<AMAX;i++) { 
+	for(i=0; i < AMAX; i++) { 
 		astb[i].state = 4; astb[i].ctr = 0;
 		aclk[i].state = 4; aclk[i].ctr = 0;
 		adat[i].state = 4; adat[i].ctr = 0; 
 	}
-	for(i=0;i<AMAX;i++) {
-	  ts   = esp_timer_get_time();
+	printf("  STB      CLK      DI/O \n");
+	for(i = 0; i < AMAX; i++) {
+		t = time(NULL);
+		tm = *localtime(&t);
+
+		ts   = esp_timer_get_time();
 	  stb  = (GPIO.in >> strobe_pin ) & 0x1; //gpio_get_level( strobe_pin ); // faster pin read https://pastebin.com/raw/AT9Z6hEJ
 	  // implementasi sederhana deteksi RISING dan FALLING: 00 = LOW , 01=RISING, 10=FALING, 11=HIGH
-	  stbs =  stb  | stb0 << 1; // 
-	  if ( astb[ cstb ].state == stbs ) // 
-	    astb[ cstb ].ctr++;
-	  else {
-	    astb[ cstb ].state = stbs;
-		astb[ cstb ].ctr++;
-	  }  
-	  if ( stb != stb0 ) cstb++;  //
+	  stbs =  stb | (stb0 << 1); // 
+
+		/*
+		bit-shift diatas diimplementasi dalam bentuk yang tidak efisien dalam 4 baris ini.
+
+		if (stb == 1 && stb0 == 0) stbs = 01;
+		if (stb == 0 && stb0 == 1) stbs = 10;
+		if (stb == 1 && stb0 == 1) stbs = 11;
+		if (stb == 0 && stb0 == 0) stbs = 00;
+		*/
+
 	  stb0 = stb;
 
-	  //ets_delay_us( 1 );
 	  tc   = esp_timer_get_time();
 	  clk  = gpio_get_level( clock_pin ); // digitalRead
-	  clks =  clk | clk0 << 1;
-	  if ( aclk[ cclk ].state == clks ) // 
-	    aclk[ cclk ].ctr++;
-	  else {
-	    aclk[ cclk ].state = clks;
-		aclk[ cclk ].ctr++;
-	  }  
-	  if ( clk != clk0 ) cclk++;  //
+	  clks =  clk | (clk0 << 1);
+	 
+	  if ( clk != clk0 ) cclk++; 
 	  clk0 = clk;
-	  //ets_delay_us( 1 );
 
 	  td   = esp_timer_get_time();
 	  dat  = gpio_get_level( data_pin ); // 50ns https://www.reddit.com/r/esp32/comments/f529hf/results_comparing_the_speeds_of_different_gpio/
-      dats =  dat | dat0 << 1;
-	  if ( adat[ cdat ].state == dats ) // 
-	    aclk[ cdat ].ctr++;
-	  else {
-	    aclk[ cdat ].state = dats;
-		aclk[ cdat ].ctr++;
-	  }  
-	  if ( dat != dat0 ) cdat++;  //
-      dat0 = dat;
-	  //ets_delay_us( 1 );
+    dats =  dat | (dat0 << 1);
+	  
+    dat0 = dat;
+	  
 	  timer_start = esp_timer_get_time();
-	  //if( ctr % 1000 == 0 )
-	   printf( "%d  %d  %d %6s  %6s  %6s %6lld %6lld %6lld \n", stb, clk, dat, state[stbs], state[clks], state[dats],  tc-ts, td-tc, timer_start-td )	 ;
+
+		gettimeofday(&tv_now, NULL);
+		time_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+		
+	  printf( "%d  %6s %d %6s %d %6s %6lld %6lld %6lld %d-%02d-%02d %02d:%02d:%02d:%2lld\n", stb, state[stbs], clk, state[clks], dat, state[dats],  tc-ts, td-tc, timer_start-td, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, time_us)	 ;
 	  ts =  timer_start;
 	  ctr++;
 	}
 	
-	for(i=0;i<AMAX;i++) { 
-	   printf( "%4d %6s  %4d  %6s  %4d %6s  %4d \n", i,state[ astb[ i ].state ], astb[ i ].ctr, 
-	     state[ aclk[ i ].state], aclk[ i ].ctr, 
-		 state[ adat[ i ].state ], adat[ i ].ctr );
+	for(i = 0; i < AMAX; i++) { 
+		t = time(NULL);
+	  printf( "%4d %6s \t  %4d %6s \t  %4d %6s  %4d \n", i, state[ astb[ i ].state ], astb[ i ].ctr, state[ aclk[ i ].state], aclk[ i ].ctr, state[ adat[ i ].state ], adat[ i ].ctr );
 	   //printf( "%4d %6d  %4d  %6d  %4d %6d  %4d \n", i,  astb[ i ].state , astb[ i ].ctr,  aclk[ i ].state, aclk[ i ].ctr,  adat[ i ].state , adat[ i ].ctr );
 	}
-
-
 }
 
 #include "soc/rtc_wdt.h"
@@ -248,7 +250,7 @@ void read_bitbybit(void *pvParameter) {
 #include "esp_task_wdt.h"
 
 void app_main(void) {
-	//app_main2(  );
+	//app_main2();
 	// disable watchdog
   rtc_wdt_protect_off();
   rtc_wdt_disable();
@@ -258,20 +260,20 @@ void app_main(void) {
   //init_sdcard();
 	isr_io_config();
 	/*
-    gpio_reset_pin( clock_pin );    
+  gpio_reset_pin( clock_pin );    
 	gpio_set_direction( clock_pin, GPIO_MODE_INPUT ); 
 	//gpio_set_pull_mode(clock_pin, GPIO_PULLUP_ONLY);
 
-    gpio_reset_pin( data_pin );     gpio_set_direction( data_pin, GPIO_MODE_INPUT); 
-    gpio_reset_pin( strobe_pin );     gpio_set_direction( strobe_pin, GPIO_MODE_INPUT); 
-*/
+  gpio_reset_pin( data_pin );     gpio_set_direction( data_pin, GPIO_MODE_INPUT); 
+  gpio_reset_pin( strobe_pin );     gpio_set_direction( strobe_pin, GPIO_MODE_INPUT);
+	*/
 
-    BaseType_t xReturned;
-    xReturned = xTaskCreate( &read_bitbybit, "read_bitbybit (name)", 2048, NULL, RTOS_TASK_PRIORITY_NORMAL, NULL);
-    if (xReturned == pdPASS) {  printf( "OK Task has been created, and is running right now\n"); }
+  BaseType_t xReturned;
+  xReturned = xTaskCreate( &read_bitbybit, "read_bitbybit (name)", 2048, NULL, RTOS_TASK_PRIORITY_NORMAL, NULL);
+  if (xReturned == pdPASS) {  printf( "OK Task has been created, and is running right now\n"); }
 	read_bitbybit( NULL );
 	while(1) {
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
 	}
 
 }
