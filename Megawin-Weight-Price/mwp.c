7df11724120b83e2ad20c69b9196bf51735afb97
Umar <umarudy@outlook.co.id>
2021-06-01 11:53:01 +0000

    Success get weight, price, total from Megawin

--- Megawin-Weight-Price/mwp.c
+++ Megawin-Weight-Price/mwp.c
@@ -43,60 +43,41 @@ int car = 0;
 
 int main(){
 	if (!bcm2835_init()) return 1;
-    bcm2835_gpio_fsel(STB, BCM2835_GPIO_FSEL_INPT); 
+	bcm2835_gpio_fsel(STB, BCM2835_GPIO_FSEL_INPT); 
 	bcm2835_gpio_fsel(CLK, BCM2835_GPIO_FSEL_INPT); 
-    bcm2835_gpio_fsel(DIO, BCM2835_GPIO_FSEL_INPT); 
-	
+	bcm2835_gpio_fsel(DIO, BCM2835_GPIO_FSEL_INPT); 
 	pthread_t t;
 	pthread_create( &t, NULL, printEvent, NULL ); 
 	eventCount();
-	
 	return 0;
 }
+
 int shbf_done = 0;
 
 void* printEvent(void *arg){	
-	/*
-	if (ar[0] == 3 && ar[1] == 64){
-		printf("0: %3d 1:%3d 2:%3d \n", ar[0], ar[1], ar[2]); fflush( stdout );
-	}
-	*/
 	while(1){
 		sleep(0.1);
 		if (car < 54) continue;
 		car = 0;
 		if (ar[17] != 138 || ar[5] < 115 ) continue;
-		
-		//if ( /*ar[3] != 0 ||*/ ar[6] != 0 || ar[8] != 0 || ar[10] != 0 ) continue; // element 6,8,10 wajib = 0
 		/*
 		for (int i = 0; i < 18; i++){	
-			printf("%3d ", ar[i] );
-			/*
 			for (int j = 0; j < 20; j++)
-				if (ar[i] == full_digits[j]) {
-					printf("%d ", j%10 );
-				}
-				
+				if (ar[i] == full_digits[j]) printf("%d ", j%10 );
+		}
+		printf("\t");
+		for (int i = 19; i < 19+16; i++){	
+			for (int j = 0; j < 20; j++)
+				if (ar[i] == full_digits[j]) printf("%d ", j%10 );
 		}
 		*/
-		
-		
-
-		//for (int i = 0; i < 18; i++){	
-		for (int i = 0; i < 18; i++){	
-			//printf("%3d ", ar[i] );
-			
+		for (int i = 0; i < 54; i++){	
 			for (int j = 0; j < 20; j++)
-				if ((ar[i] == full_digits[j] && ar[13] == 2) || (ar[i] == full_digits[j] && ar[13] == 3)) {
-					printf("++%d ", j%10 );
-				}
-				
-				
+				if (ar[i] == full_digits[j]) printf("%d ", j%10 );
+			if (i % 18 == 0) printf("\t");
 		}
 		
-		printf("\n"); fflush( stdout );
-
-		//shbf_done = 0;
+		printf("\r"); fflush( stdout );
 	}
 
 }
@@ -108,7 +89,8 @@ void eventCount(){
 		clk_dat = bcm2835_gpio_lev( CLK );
 		stb_state = setState(stb_dat, g_s_prev, &stb_pin, &g_s_prev, &stb_pin_prev);
 		clk_state = setState(clk_dat, g_c_prev, &clk_pin, &g_c_prev, &clk_pin_prev);
-		if (stb_pin_prev.ctr > 40000 && stb_pin.state == FALLING) shbf_done = 1;	// dengan baris 117 ini seperti logika AND yang artinya memperhatikan proses awal dan akhir pengambilan data		
+		
+		if (stb_pin_prev.ctr > 40000 && stb_pin.state == FALLING) shbf_done = 1;			
 		if (shbf_done == 1 && clk_pin.state == RISING){
 			dio_dat = bcm2835_gpio_lev( DIO );
 			if (ctr_dio > 7){
@@ -116,23 +98,8 @@ void eventCount(){
 				ctr_dio = 0; value = 0; 
 			}
 			value = value | (dio_dat << ctr_dio++);
-			if ( (car+1) % 54 == 0 && car != 0 ) { shbf_done = 0; ; };
+			if ( (car+1) % 54 == 0 && car != 0 ) shbf_done = 0;
 			
 		}
 	}
 }
-/*
- eventCount mengumpulkan bit sampai jadi decimal. Decimalnya kumpulin di array.
- Print decimal yang di array ketika :
- a. STB persiapan ke cycle berikutnya
- b. jumlah array sudah mencapai jumlah sevseg
- 
- Problemnya, ngeprint nggak konstan (ketambahan angka yang tidak seharusnya).
- Angka yang panjangnya nggak sesuai sevseg tetep diprint, dan ditimpakan ke ujung kiri. 
- Atau ketika panjangnya melebihi sevseg, juga tetep diprint. Jadi kelihatan panjang banget outputnya. Dan nggak sesuai.
-
- Untuk mencegah kejadian itu:
- 1. Agka yang perlu diprint adalah angka yang ada di antara header dan footer 
- 2. Kalau angka ke 18 adalah 138 dan angka pertamanya 3, print angka ditengahnya. Atau kalau angka pertama bukan 3 dan angka ke 18 bukan 138 abaikan sisanya. 
- 3. CAR ditambahkan hanya ketika valuenya lebih dari 114 (masuk dalam digits).
-*/
